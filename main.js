d81a644227c029f21c799d9408b77b9c7de3e482
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-02-25 16:37:47 +0700

    lesson 7 is not completed

--- main.js
+++ main.js
@@ -2,19 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green.jpg",
-        inStock: false,
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "Green",
-                variantImage: "./assets/vmSocks-green.jpg"
+                variantImage: "./assets/vmSocks-green.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "Blue",
-                variantImage: "./assets/vmSocks-blue.jpg"
+                variantImage: "./assets/vmSocks-blue.jpg",
+                variantQuantity: 0
             }
         ],
         cart: 0
@@ -23,8 +25,20 @@ var app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct: function(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 })
